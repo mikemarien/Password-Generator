49e6e2aa35a940d573a8d278da70e7545a01405e
Mike Marien <mikemarien@cz.local>
2020-06-13 00:00:12 -0500

    message

--- README.md
+++ README.md
@@ -1,3 +1,51 @@
 # Password-Generator
 
-This is a test of the read me.
+As an employee with access to sensitive data, I have taken great care to create a simple, but effective password generator. 
+
+I have created a password generator that gives the user the option of changing the length, whether or not the are upper or lowercase letters, whether or not there are symbols, and whether or not there are numbers. 
+
+This password generator improves the security of our company. 
+
+I went through great effort to create this password generator. 
+
+In the future I should add more commits, rather commit all at once. Small commits are important for working on a team, so that it's clear, if there are bugs, what commits caused the bugs. 
+
+
+I used the Paletton (http://paletton.com/#uid=13u0u0kCAoD6YC+qwuUP5gT+h4k) color tool to create a color scheme. I was inspired by family vacations to the Badlands of south Dakota, particularly the many shades of dust and dirt in the desert sunlight. 
+
+I deleted a lot of css, on the bases that whether or not it was present, nothing changed in the GUI. 
+In the future I'd like to examine if the CSS I deleted had an effect that was not obvious.
+
+My biggest unsolved issue in this has been proper formatting of the checkboxes.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- script.js
+++ script.js
@@ -1,44 +1,31 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+function generate() {
+  let password = "";
+  let length = document.getElementById("length").value;
+  let lowerCase = document.getElementById("lowercase").checked;
+  let upperCase = document.getElementById("uppercase").checked;
+  let symbol = document.getElementById("symbols").checked;
+  let number = document.getElementById("numbers").checked;
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  if (lowerCase + upperCase + symbol + number <= 0)
+      return;
 
-  passwordText.value = password;
+  for (let i = 0; i < length; i++) {
+
+      const r = generator(0, 3);
+      if (lowerCase && r === 0) {
+          password += randomLowerCase();
+      } else if (upperCase && r === 1) {
+          password += randomRandomUpperCase();
+      } else if (symbol && r === 2) {
+          password += randomRandomSymbol();
+      } else if (number && r === 3) {
+          password += generator(0, 9);
+      } else {
+          i--;
+      }
+
+  }
+
+  document.getElementById("result").textContent = password;
 
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-//password criteria
-
-function randomSymbol() {
-  return String.fromCharCode(Math.floor(Math.random() * 14) + 33);
-  //retrives html character code, math.floor sets math.random to integers rather than decimals. 14 is the number of symbols that are being used. 33 sets the range the symbols begin from in the character code.
-}
-
-function randomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-  //retrives html character code, math.floor sets math.random to integers rather than decimals. 10 is the number of numbers. 48 sets the range where the numbers begin from in the character code.
-}
-
-function randomLowercase() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-  //retrives html character code, math.floor sets math.random to integers rather than decimals. 26 is number of letters in alphabet. 97 sets the range where lower case letters begin.
-}
-
-function randomUppercase() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-  //retrives html character code, math.floor sets math.random to integers rather than decimals. 26 is number of letters in alphabet. 65 sets the range where upper case letters begin.
-}
-
-const passwordCriteria = {
-  symbol: randomSymbol,
-  number: randomNumber,
-  lowercase: randomLowercase,
-  uppercase: randomUppercase
-};
-
